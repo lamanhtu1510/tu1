2220b9ff7ddd54cf9fce08d08a50b901ecff1359
YOUR NAME <themen1510@gmail.com>
2016-06-05 20:24:59 +0700

    lan 3

--- JavaApplication51/src/javaapplication51/JavaApplication51.java
+++ JavaApplication51/src/javaapplication51/JavaApplication51.java
@@ -17,7 +17,7 @@ public class JavaApplication51 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("I love you");
-        System.out.println("hoho");
+        System.out.println("hhaerhoa");
     }
     
 }
